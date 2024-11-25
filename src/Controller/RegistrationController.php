8adf8cff4a60e9d3ea2867db11ae2ebba6d51627
Kythadrin <vadimsiric@gmail.com>
2024-11-25 06:35:41 +0200

    Code cleaning

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -10,19 +10,8 @@ use Twig\Environment;
 
 class RegistrationController extends AbstractController
 {
-    public function __construct(
-        private readonly UserService $userService,
-        private readonly EntityManagerInterface $entityManager,
-        Environment $twig,
-    ) {
-        parent::__construct($twig);
-    }
-
     public function index(): void
     {
-        $this->userService->registrate('test@mail.com', '123');
-        $this->entityManager->flush();
-
         $this->render('registration.html.twig',
         [
             "pageClass" => 'registration',
